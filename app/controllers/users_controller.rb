606aea227024c86847bd834861d170de901fca71
kyosuke ichimua <sr030629@gmail.com>
2020-06-02 11:55:25 +0000

    ユーザー登録機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
-class UsersController < ApplicationController
-  def new
+def create
+  @user = User.new(name: params[:user][:name], email: params[:user][:email])
+  if @user.save
+    redirect_to root_path
+  else
+    render :new
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
 end
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+end
