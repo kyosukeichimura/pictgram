0ac5949b5f0395b9fb6aabe463edc600f79b15f5
kyosuke ichimua <sr030629@gmail.com>
2020-06-02 12:54:04 +0000

    バリデーション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
-def create
-  @user = User.new(name: params[:user][:name], email: params[:user][:email])
-  if @user.save
-    redirect_to root_path
-  else
-    render :new
+class UsersController < ApplicationController
+  def new
+    @user = User.new
   end
-end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, success: '登録が完了しました'
+    else
+      flash.now[:danger] = "登録に失敗しました"
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end 
+
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-end
-class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
 end
