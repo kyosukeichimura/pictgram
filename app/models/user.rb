b8d46b4f6a7a27f6fb4da773e6a2a4337d664166
kyosuke ichimua <sr030629@gmail.com>
2020-06-06 13:53:25 +0000

    課題バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true,length:{maxmum:15}
   validates :email, presence: true
   has_secure_password
+  VALID_PASSWORD_REGEX=/\A(?=.*[a-z])(?=.*?\d)[!-~]+\z/i
+  validates :password,length:{minimum:8,maxmum:32}
+  VALID_EMAIL_REGEX=/\A[.]+@[.]+\.[.]+\Z/i
 end
