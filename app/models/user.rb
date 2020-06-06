ca1ea91f0f9f3205058fa478f7f4523d398bbf28
kyosuke ichimua <sr030629@gmail.com>
2020-06-06 14:30:12 +0000

    ナビゲーション機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true,length:{maxmum:15}
+  validates :name, presence: true, length: {maximum:15}
   validates :email, presence: true
   has_secure_password
   VALID_PASSWORD_REGEX=/\A(?=.*[a-z])(?=.*?\d)[!-~]+\z/i
-  validates :password,length:{minimum:8,maxmum:32}
+  validates :password,length:{minimum:8,maximum:32}
   VALID_EMAIL_REGEX=/\A[.]+@[.]+\.[.]+\Z/i
 end
