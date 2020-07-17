a084c78d924010651fa90ceedddc2930c07d71dd
kyosuke ichimua <sr030629@gmail.com>
2020-07-17 03:34:47 +0000

    いいね機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :password, format: { with: VALID_PASSWORD_REGEX }
   has_many :topics
   has_many :favorites
+  has_many :favorite_topics, through: :favorites, source: 'topic'
 end
